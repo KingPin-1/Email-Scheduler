38cd02c746db6a6f7d32f1357ebe97c8fa42eeeb
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-01-31 10:54:01 +0530

    Complete Project ... Just need to test with send in blue

---
+++ config/credentials.js
@@ -0,0 +1,7 @@
+const auth = {
+    USER_EMAIL: "mohdathar1991@gmail.com",
+    USER_PASSWORD:
+        "xsmtpsib-ff7d78c0e4e720c744a85607d60a6f3dd14adc7547b4af0d6120cd8729ebbea2-DNWFJKz8TnOQj96v",
+};
+
+module.exports = auth;
--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const app = express();
+const scheduler = require("node-cron");
+const { transporter, options } = require("./services/email");
 
 const PORT = 1337;
+
+// log every second --> ex of node-cron.schedule()
+// scheduler.schedule("* * * * * *",() => {
+//     console.log('In schedule');
+// })
+
+scheduler.schedule("* * * * * *", () => {
+    console.log("inside Scheduler");
+    transporter.sendMail(options, (err, info) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("Email send with info = ", info);
+        }
+    });
+});
 
 app.listen(PORT, () => {
     console.log(`Listening on Port : ${PORT}`);
