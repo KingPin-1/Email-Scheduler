536d22133d8e60183fcb3341586c23c0ed7bf655
Athar M <93383101+KingPin-1@users.noreply.github.com>
2023-02-01 09:10:27 +0530

    Merged from BUG
    * Base replit stuff
    
    * replit state complete
    
    * Minor Changes
    
    * Updated README

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Email-Scheduler
 Node js Backend application to schedule and automate emails using Node cron and Nodemailer.
+
+Try here:
+https://replit.com/@AtharM/Email-Scheduler#index.js
+
+Steps ( to send as outlook user email ) :
+Go to config > credentials and change user email and password to match yours
+Change subject and CC, BCC , text and html as needed
+add RECEIVER_EMAIL
+In index.js > schedule() modify string parameter to suit your cron need
+Check out http://crontab.guru/ to test out suitable schedules.
--- services/email.js
+++ services/email.js
@@ -5,12 +5,13 @@ const { USER_EMAIL, USER_PASSWORD } = require("../config/credentials");
 // const HOST_SERVICE = "smtp-relay.sendinblue.com"; Couldn't get send in blue to work
 
 const SENDER_EMAIL = USER_EMAIL;
-const RECEIVER_EMAIL = "mohdathar1991@gmail.com";
+const RECEIVER_EMAIL = "receiver_email@provider.com";
 
 const CC = [];
 const BCC = [];
 
 const EMAIL_SUB = "HAPPY BIRTHDAY my friend";
+const EMAIL_TEXT = "AUTOMATED EMAIL SENT TO WISH YOU A HAPPY BIRTHDAY";
 const EMAIL_HTML_BODY = "<h1>Happy Birthday!!!</h1>";
 
 const options = {
@@ -19,11 +20,12 @@ const options = {
     cc: CC,
     bcc: BCC,
     subject: EMAIL_SUB,
+    text: EMAIL_TEXT,
     html: EMAIL_HTML_BODY,
 };
 
 const transporter = nodemailer.createTransport({
-    service: "hotmail",
+    service: "hotmail", // if user email is outlook for others see doc
     auth: {
         user: USER_EMAIL,
         pass: USER_PASSWORD,
